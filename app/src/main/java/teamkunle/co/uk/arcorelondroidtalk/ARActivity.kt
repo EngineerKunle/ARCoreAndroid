c97ff3a434dd12ca1c7274fd8e1ea79dbea1011d
Kunle Ogunjimi <kunle.ogunjimi@telegraph.co.uk>
2018-08-12 00:43:07 +0100

    updated method

--- app/src/main/java/teamkunle/co/uk/arcorelondroidtalk/ARActivity.kt
+++ app/src/main/java/teamkunle/co/uk/arcorelondroidtalk/ARActivity.kt
@@ -1,10 +1,13 @@
 package teamkunle.co.uk.arcorelondroidtalk
 
 import android.graphics.Point
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import com.google.ar.core.*
 import com.google.ar.sceneform.ux.ArFragment
 import kotlinx.android.synthetic.main.activity_ar.*
@@ -41,8 +44,8 @@ class ARActivity : AppCompatActivity() {
     }
 
     private fun onUpdate() {
-        val trackingChanged : Boolean = updateTracking()
-        val view : View = findViewById(android.R.id.content)
+        val trackingChanged: Boolean = updateTracking()
+        val view: View = findViewById(android.R.id.content)
 
         if(trackingChanged) {
             if(isTracking) {
@@ -64,7 +67,7 @@ class ARActivity : AppCompatActivity() {
     }
 
     private fun updateTracking(): Boolean {
-        val frame : Frame = fragment.arSceneView.arFrame
+        val frame: Frame = fragment.arSceneView.arFrame
 
         var wasTracking: Boolean = isTracking
         isTracking = frame.camera.trackingState == TrackingState.TRACKING
@@ -72,10 +75,10 @@ class ARActivity : AppCompatActivity() {
     }
 
     private fun updateHitTest(): Boolean {
-        val frame : Frame = fragment.arSceneView.arFrame
+        val frame: Frame = fragment.arSceneView.arFrame
         var pt = getScreenCenter()
 
-        var hits : List<HitResult>
+        var hits: List<HitResult>
         var wasHitting : Boolean = isHitting
         isHitting = false
 
@@ -99,4 +102,44 @@ class ARActivity : AppCompatActivity() {
         return Point(view.width / 2, view.height / 2)
     }
 
+    //initialise the gallery view
+    private fun initGallery() {
+        val gallery: LinearLayout = findViewById(R.id.gallery_layout)
+
+        val andy  = ImageView(this)
+        val cabin = ImageView(this)
+        val house = ImageView(this)
+        val igloo = ImageView(this)
+
+        //droid
+        andy.setImageResource(R.drawable.droid_thumb)
+        andy.contentDescription = "andy"
+        andy.setOnClickListener { addObject(Uri.parse("andy.sfb")) }
+        gallery.addView(andy)
+
+        //cabin
+        cabin.setImageResource(R.drawable.cabin_thumb)
+        cabin.contentDescription = "cabin"
+        cabin.setOnClickListener { addObject(Uri.parse("Cabin.sfb")) }
+        gallery.addView(cabin)
+
+        //House
+        house.setImageResource(R.drawable.house_thumb)
+        house.contentDescription = "House"
+        house.setOnClickListener{ addObject(Uri.parse("House.sfb"))}
+        gallery.addView(house)
+
+        //igloo
+        igloo.setImageResource(R.drawable.igloo_thumb)
+        igloo.contentDescription = "igloo"
+        igloo.setOnClickListener { addObject(Uri.parse("igloo.sfb"))}
+        gallery.addView(igloo)
+
+    }
+
+    //TODO : finish code
+    private fun addObject(model: Uri?) {
+        val frame = fragment.arSceneView.arFrame
+
+    }
 }
